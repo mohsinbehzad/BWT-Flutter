b83ea14673261ee6758ca83c98d80f5217a42332
mohsinexe <mohsinbehzad14@gmail.com>
2024-07-05 09:15:49 +0500

    App Bar addded

--- Week 2/Task_1/lib/basic_widgets/app_bar.dart
+++ Week 2/Task_1/lib/basic_widgets/app_bar.dart
@@ -1,10 +1,11 @@
 // app bar is used on top or bottom, both have different implementation.
 // 3 components of app bar are leading, title and actions.
 
+import 'package:basic_widgets/basic_widgets/container.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class APP_BAR extends StatelessWidget {
+  const APP_BAR({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -12,18 +13,18 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.amber,
 
-        toolbarHeight: 150,
-        toolbarOpacity: 0.5,                                                      //value should be 0 to 1,and it is not applied to appbar. it works on elements of Appbar like title etc
+        // toolbarHeight: 150,
+        // toolbarOpacity: 0.5,                                                      //value should be 0 to 1,and it is not applied to appbar. it works on elements of Appbar like title etc
 
         elevation: 25,                                                      //shadow below appbar
         shadowColor: Colors.red,
 
         leading: const Icon(Icons.menu),
-        leadingWidth: 300,
+        leadingWidth: 50,
 
         title: const Text('AppBar'),
         titleTextStyle: const TextStyle(
-          color: Colors.deepPurpleAccent,
+          color: Colors.purple,
           fontSize: 28,
         ),
 
@@ -37,12 +38,20 @@ class HomeScreen extends StatelessWidget {
         actions: const [
           Icon(Icons.more_vert),
           Icon(Icons.search),
-          Icon(Icons.pages),
         ],
         actionsIconTheme: const IconThemeData(color: Colors.amber, size: 26),
       ),
-      
-      drawer: Drawer(),
+      drawer: const Drawer(),
+
+      body: InkWell(
+          onTap: (){
+            Navigator.push(context, 
+            MaterialPageRoute(builder: (context) => const BasicContainer()));
+          },
+          child: const Center(
+            child: Text('Container'),
+          ),
+      ),
     );
   }
 }
