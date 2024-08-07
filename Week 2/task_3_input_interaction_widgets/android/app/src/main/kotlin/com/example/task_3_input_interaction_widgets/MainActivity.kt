4a94d84ec80be26da137394b8f29ab02d28ca47a
mohsinbehzad <mohsinbehzad14@gmail.com>
2024-08-07 05:45:10 +0500

    Input and Interactive Widgets

---
+++ Week 2/task_3_input_interaction_widgets/android/app/src/main/kotlin/com/example/task_3_input_interaction_widgets/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.task_3_input_interaction_widgets
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ Week 2/task_3_input_interaction_widgets/lib/input_widgets.dart/autocomplete.dart
@@ -0,0 +1,47 @@
+// Autocomplete
+
+import 'package:flutter/material.dart';
+import 'package:task_3_input_interaction_widgets/input_widgets.dart/form.dart';
+
+class InputWidgets extends StatelessWidget {
+  const InputWidgets({super.key});
+
+  static const List<String> listItems = <String>['Peas', 'Potatoe', 'Tomatoe'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        actions: <Widget> [
+          GestureDetector(
+            onTap: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context) => InputWidgets2()));
+            },
+            child: const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Icon(Icons.widgets),
+              
+            ),
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Autocomplete<String>(
+          optionsBuilder: (TextEditingValue textEditingValue) {
+            if (textEditingValue.text == '') {
+              return const Iterable<String>.empty();
+            }
+            return listItems.where((String item) {
+              return item.contains(textEditingValue.text.toLowerCase());
+            });
+          },
+          onSelected: (String item) {
+            print('The $item is selected.');
+          },
+        ),
+      ),
+    );
+  }
+}
